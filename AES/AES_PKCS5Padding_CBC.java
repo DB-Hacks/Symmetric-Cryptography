73085d449aea546684af7c1d81f2d12aeeea3688
DB-Hacks <Daniel.Brown.1396@gmail.com>
2020-01-13 20:56:57 -0500

    Updated AES_PKCS5Padding_CBC.java
    Incorporated AES_PKCS5Padding_CBC_Decrypt.java
    into this singular program

--- AES/AES_PKCS5Padding_CBC.java
+++ AES/AES_PKCS5Padding_CBC.java
@@ -1,4 +1,4 @@
-package symmetric;
+package AES;
 
 import java.security.Key;
 import java.security.spec.AlgorithmParameterSpec;
@@ -7,28 +7,26 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import util.CryptoTools;
+import Utility.CryptoTools;
 
 public class AES_PKCS5Padding_CBC {
 	public static void main(String[] args) throws Exception
 	{
-		byte[] ct = CryptoTools.hexToBytes("F38ADBA8A7B4CC613578355032205D50");
-		byte[] ky = CryptoTools.hexToBytes("9F0DCEDB322F3C6873F9256E01376BA4");
-		byte[] iv = CryptoTools.hexToBytes("20FC19123087BF6CAC8D0F1254123004");
+		byte[] pt = "Computer Security program at York University".getBytes();
+		byte[] ky = "CONFIDENTIALITY!".getBytes();
+		byte[] iv = CryptoTools.hexToBytes("13AD19123087BF6CAC8D0F1254128772");
 		
 		Key secret = new SecretKeySpec(ky, "AES");
 		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		AlgorithmParameterSpec aps = new IvParameterSpec(iv);
+		cipher.init(Cipher.ENCRYPT_MODE, secret, aps);
+		byte[] ct = cipher.doFinal(pt);
+		
+		System.out.println("CT = " + CryptoTools.bytesToHex(ct));	//Test Case: 7A937C4387F5C719811C219FE1AB1E327A241CAE934C43EDE359A9AC9A1A3DA538AFFD28CB4F398B0EBB8428BA730EC6
+		
 		cipher.init(Cipher.DECRYPT_MODE, secret, aps);
 		byte[] bk = cipher.doFinal(ct);
 		
-		System.out.println("BK = " + new String(bk) + "<");
-		
-		cipher.init(Cipher.ENCRYPT_MODE, secret, aps);
-		byte[] ck = cipher.doFinal(bk);
-		System.out.println("CT = " + CryptoTools.bytesToHex(ck));
-		
-		//First Word "The"
-
+		System.out.println("BK = " + new String(bk) + "<");		//Test Case: Computer Security program at York University<
 	}
 }
