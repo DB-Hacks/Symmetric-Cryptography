304dd5830e630ac1d98c5ca3c55ad6c5fa4c8594
DB-Hacks <Daniel.Brown.1396@gmail.com>
2020-01-13 21:46:41 -0500

    Updated DES_NoPadding_ECB.java

--- DES/DES_NoPadding_ECB.java
+++ DES/DES_NoPadding_ECB.java
@@ -1,31 +1,31 @@
-package symmetric;
+package DES;
 
 import java.security.Key;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
-import util.CryptoTools;
+import Utility.CryptoTools;
 
 public class DES_NoPadding_ECB
 {
 
 	public static void main(String[] args) throws Exception
 	{
-		byte[] pt = "Hello World. My ".getBytes();
-		byte[] ky = CryptoTools.hexToBytes("34567abcdef03211");
+		byte[] pt = "ComputerSecurity".getBytes();
+		byte[] ky = CryptoTools.hexToBytes("50927abcdef14092");
 		
 		Key secret = new SecretKeySpec(ky, "DES");
 		Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
 		cipher.init(Cipher.ENCRYPT_MODE, secret);
 		byte[] ct = cipher.doFinal(pt);
 		
-		System.out.println("CT = " + CryptoTools.bytesToHex(ct));
+		System.out.println("CT = " + CryptoTools.bytesToHex(ct));	//Test Case: 1AE55F40610292E7A83EBA321B213894
 		
 		cipher.init(Cipher.DECRYPT_MODE, secret);
 		byte[] bk = cipher.doFinal(ct);
-		System.out.println("BK = " + new String(bk) + "<");
 		
+		System.out.println("BK = " + new String(bk) + "<");		//Test Case: ComputerSecurity<
 
 	}
 
