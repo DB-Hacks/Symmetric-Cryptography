aef3bac2792859fc6e3f8f052beb8e9fee89a6a3
DB-Hacks <Daniel.Brown.1396@gmail.com>
2020-01-14 22:10:44 -0500

    Updated OTP.java

--- OTP/OTP.java
+++ OTP/OTP.java
@@ -1,37 +1,26 @@
-package symmetric;
+package OTP;
 
-import util.CryptoTools;
+import Utility.CryptoTools;
 
 public class OTP {
 	public static void main(String[] args) throws Exception
 	{
-		byte [] pt = ("SENDMOREMONEY").getBytes();
-		byte [] ky = ("JABHXPVOLLCIJ").getBytes();
-		byte [] cash = ("CASHNOTNEEDED").getBytes();
-		byte [] ct = new byte[pt.length];
-		for (int i = 0; i < pt.length; i++) {
-			ct[i] = (byte) (ky[i]^pt[i]);
-		}
+		byte [] pt = ("Meet me at Lane on Sunday").getBytes();
+		byte [] ky = ("JABHXPVOLLCIJSDFLJKSADFDF").getBytes();
+		byte [] rt = ("Meet me at Berg on Friday").getBytes();
 		
-		System.out.println("PT = " + new String(pt));
-		System.out.println("KY = " + new String(ky));
-		//System.out.println("CT = " + new String(ct));
+		byte [] ct = CryptoTools.getXor(ky, pt);
+		System.out.println("CT = " + new String(ct));	//Test Case: $'<x=3o-8c+=!f#$k
 		
-		//decrypt
-		byte [] bk = new byte[ct.length];
-		for(int i = 0;i < pt.length; i++) {
-			bk[i] = (byte) (ky[i]^ct[i]);
-		}
-		System.out.println("BK = " + new String(bk));
 		
-		//random key
-		byte [] test = CryptoTools.getXor(cash, ct);
-		System.out.println("Random ky = " + new String(test));
-		byte [] bk2 = CryptoTools.getXor(test, ct);
-		System.out.println("Random bk = " + new String(bk2));
+		byte [] bk = CryptoTools.getXor(ky, ct);
+		System.out.println("BK = " + new String(bk));	//Test Case: Meet me at Lane on Sunday
 		
-		// to get from single-core time to cluster convert it to one hour than multiple 2.15 to get the ms in cluster
-		// flip a random bit in pt and if half or more of the ct changes its a block, stream uses xor
-		// mono: x = y, poly: x1, x2, x3, ..., xn = y
+		//A wrong key that results in another proper English sentence
+		byte [] Wrong_ky = CryptoTools.getXor(rt, ct);
+		System.out.println("Wrong ky = " + new String(Wrong_ky));	//Test Case: JABHXPVOLLCGNOFFLJKFFCFDF
+		
+		byte [] bk2 = CryptoTools.getXor(Wrong_ky, ct);
+		System.out.println("Wrong bk = " + new String(bk2));	//Test Case: Meet me at Berg on Friday
 	}
 }
